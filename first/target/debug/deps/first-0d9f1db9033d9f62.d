9980e0581c01c1b27f00c5e9015a5ef9d25e89d7
mj010101 <minjun4799@gmail.com>
2025-02-13 20:11:42 +0900

    initial commit

---
+++ first/target/debug/deps/first-0d9f1db9033d9f62.d
@@ -0,0 +1,5 @@
+/Users/minjun/Desktop/solana_bootcamp/first/target/debug/deps/libfirst-0d9f1db9033d9f62.rmeta: src/main.rs
+
+/Users/minjun/Desktop/solana_bootcamp/first/target/debug/deps/first-0d9f1db9033d9f62.d: src/main.rs
+
+src/main.rs:
