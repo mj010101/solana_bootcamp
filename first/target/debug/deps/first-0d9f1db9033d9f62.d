4e195c7076dd09456c74bbea57d09de23a8e7b1b
mj010101 <minjun4799@gmail.com>
2025-02-13 20:50:01 +0900

    removing placeholder

--- first/target/debug/deps/first-0d9f1db9033d9f62.d
+++
@@ -1,5 +0,0 @@
-/Users/minjun/Desktop/solana_bootcamp/first/target/debug/deps/libfirst-0d9f1db9033d9f62.rmeta: src/main.rs
-
-/Users/minjun/Desktop/solana_bootcamp/first/target/debug/deps/first-0d9f1db9033d9f62.d: src/main.rs
-
-src/main.rs:
--- first/target/debug/deps/first-ec9e2083b5055af3.d
+++
@@ -1,5 +0,0 @@
-/Users/minjun/Desktop/solana_bootcamp/first/target/debug/deps/libfirst-ec9e2083b5055af3.rmeta: src/main.rs
-
-/Users/minjun/Desktop/solana_bootcamp/first/target/debug/deps/first-ec9e2083b5055af3.d: src/main.rs
-
-src/main.rs:
